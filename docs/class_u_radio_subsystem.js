9fb5bc1b0f126e539f39020cdc4fb64031e3678a
Exequiel Pagliari <58015928+exequielpagliari@users.noreply.github.com>
2026-05-09 22:30:27 -0300

    .ADD Add Doc

--- docs/class_u_radio_subsystem.js
+++ docs/class_u_radio_subsystem.js
@@ -2,6 +2,7 @@ var class_u_radio_subsystem =
 [
     [ "LoadPlaylist", "class_u_radio_subsystem.html#a33429f88db0af600cb99fb08bdc7f63f", null ],
     [ "PlayNextTrack", "class_u_radio_subsystem.html#a26408cb6c1ba728b77983920e099e58d", null ],
+    [ "PlayPreviousTrack", "class_u_radio_subsystem.html#a343227431f6c8feae2bf0c29d3fa3459", null ],
     [ "SetRadioVolume", "class_u_radio_subsystem.html#ab1acd8be0bf4711499e978478311485b", null ],
     [ "StopRadio", "class_u_radio_subsystem.html#afc741e0f476d029417b6d8d1f94464df", null ]
 ];
--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['playnexttrack_0',['PlayNextTrack',['../class_u_radio_subsystem.html#a26408cb6c1ba728b77983920e099e58d',1,'URadioSubsystem']]]
+  ['playnexttrack_0',['PlayNextTrack',['../class_u_radio_subsystem.html#a26408cb6c1ba728b77983920e099e58d',1,'URadioSubsystem']]],
+  ['playprevioustrack_1',['PlayPreviousTrack',['../class_u_radio_subsystem.html#a343227431f6c8feae2bf0c29d3fa3459',1,'URadioSubsystem']]]
 ];
